f5508588205ccb40834a6f020db0350cd300bc5d
codemohitpra03 <codemohitprakanojia2103@gmail.com>
2024-04-28 23:26:48 +0530

    project done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
@@ -11,6 +15,10 @@ export default function Home() {
 			<HeroSection/>
       <FeaturedCourses/>
       <WhyChooseUs/>
+      <TestimonialCards/>
+      <UpcomingWebinars/>
+      <Instructors/>
+      <Footer/>
 		</main>
     </>
   );
